9a2be45fdd1d5e4df63790f7c25ebc5bdf0fd5a4
caesar <caesar.cuzco@jjay.cuny.edu>
2024-08-11 21:30:14 -0400

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-2024',
+  base: '/Portfolio-2024/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
