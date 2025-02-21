8ab2d6fd17b01aae30baed4eba5c65445adfd89f
caesarc <46427941+caesarc6@users.noreply.github.com>
2025-02-20 19:03:53 -0500

    deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,73 @@
+
 #!/usr/bin/env sh
 
 # abort on errors
 set -e
 
-# Clean the dist directory (optional)
-echo "Cleaning dist directory..."
-rm -rf dist/*
-
 # build
-echo "Building project..."
 npm run build
 
 # navigate into the build output directory
-echo "Navigating to dist directory..."
 cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo "Creating .nojekyll file..."
-echo > .nojekyll
+# echo > .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-# Initialize Git and commit changes
-echo "Initializing Git repository..."
 git init
-git checkout -B gh-pages
+git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# Push to the gh-pages branch of the specified repository
-echo "Pushing to gh-pages branch..."
-git push -f git@github.com:caesarc6/Portfolio-2024.git gh-pages
+# if you are deploying to https://<USERNAME>.github.io
+ git push -f git@github.com:caesarc6/caesarc6.github.io.git 
+main
 
-echo "Deployment complete!"
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:caesarc6/Portfolio-2024.git 
+main:gh-pages
+
 cd -
+
+# #!/usr/bin/env sh
+
+# # abort on errors
+# set -e
+
+# # Clean the dist directory (optional)
+# echo "Cleaning dist directory..."
+# rm -rf dist/*
+
+# # build
+# echo "Building project..."
+# npm run build
+
+# # navigate into the build output directory
+# echo "Navigating to dist directory..."
+# cd dist
+
+# # place .nojekyll to bypass Jekyll processing
+# echo "Creating .nojekyll file..."
+# echo > .nojekyll
+
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
+
+# # Initialize Git and commit changes
+# echo "Initializing Git repository..."
+# git init
+# git checkout -B gh-pages
+# git add -A
+# git commit -m 'deploy'
+
+# # Push to the gh-pages branch of the specified repository
+# echo "Pushing to gh-pages branch..."
+# git push -f git@github.com:caesarc6.github.io.git
+
+# echo "Deployment complete!"
+# cd -
 
 
 
